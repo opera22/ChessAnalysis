767896e0db852f314d94dc9a7ade7061d03dd58a
Drew <andrewburns.r@gmail.com>
2020-10-31 14:51:00 -0600

    added a line graph for rating vs opening moves

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
-df = pd.read_csv("games.csv", usecols=["white_rating", "black_rating", "turns", "winner"])
+df = pd.read_csv("games.csv", usecols=["white_rating", "black_rating", "turns", "winner", "opening_ply"])
+df = df.head(10000)
+
+# This bit of code creates an avg_rating column and writes it to a new csv
+avg_rating = []
+
+for index in range(len(df["white_rating"])):
+    avg = ( df["white_rating"][index] + df["black_rating"][index] ) // 2
+    avg_rating.append(avg)
+
+df["avg_rating"] = avg_rating
+
+#df = pd.read_csv("games_new.csv")
+#pd.to_csv("games_new.csv", index = True, header = True)
 
 # winner counts bar chart
 winner_counts = df["winner"].value_counts()
@@ -11,12 +24,65 @@ count_wins_black = winner_counts["black"]
 white_win_percent = count_wins_white / len(df["winner"])
 black_win_percent = count_wins_black / len(df["winner"])
 
+# white goes first in chess, so these numbers should be slightly different
 print("white wins: " + str(count_wins_white))
 print("black wins: " + str(count_wins_black))
-#difference of 894 with original dataset
 
 plt.bar(["White", "Black"], [count_wins_white, count_wins_black])
 plt.title("White wins: " + str(count_wins_white) + " Black wins: " + str(count_wins_black) + \
     "\nProbability that white wins: " + str(round(white_win_percent, 3)) + \
     "\nProbability that black wins: " + str(round(black_win_percent, 3)))   
 plt.show()
+
+# Average rating and average amount of book moves at opening ()
+df_avg_opening_ply = df.groupby(["avg_rating"])["opening_ply"].mean()
+
+df_avg_opening_ply.to_csv("games_new.csv", index = True, header = True)
+
+df_avg_opening_ply = pd.read_csv("games_new.csv")
+
+plt.plot(df_avg_opening_ply["avg_rating"], df_avg_opening_ply["opening_ply"])
+plt.xlabel("Ratings")
+plt.ylabel("Average Amt. Book Moves at Opening")
+plt.show()
+
+
+
+
+
+
+
+
+""" print(df.head(100))
+opening_ply_sums = {}
+
+for index in range(len(df["avg_rating"])):
+    try: 
+        opening_ply_sums[df["avg_rating"][index]] += 1
+    except:
+        opening_ply_sums[df["avg_rating"][index]] = 0
+
+for item in opening_ply_sums:
+
+
+
+avg_rating_avg_opening_ply = {}
+ """
+
+
+
+
+
+
+""" while continue_loop:
+    try:
+        for index in len(df["white_rating"]):
+            avg = ( df["white_rating"][index] + df["black_rating"][index] ) / 2
+            avg_rating[index] = avg
+        df["avg_rating"] = avg_rating 
+        continue_loop = False
+    except:
+        avg_rating = [] """
+
+
+
